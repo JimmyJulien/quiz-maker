2d06baaef914fce588471effbbbaa129e24c33ba
JimmyJulien <jimmy.julien@protonmail.com>
2024-04-20 18:10:52 +0200

    Signals quizz-lines

--- src/app/components/quizz-lines/quizz-lines.component.ts
+++ src/app/components/quizz-lines/quizz-lines.component.ts
@@ -1,5 +1,5 @@
 import { NgClass } from '@angular/common';
-import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
+import { ChangeDetectionStrategy, Component, input, output } from '@angular/core';
 import { QuizzAnswerModel } from 'src/app/models/quizz-answer.model';
 import { QuizzLineModel } from 'src/app/models/quizz-line.model';
 import { AnswerColorPipe } from 'src/app/pipes/answer-color.pipe';
@@ -18,38 +18,21 @@ import { AnswerColorPipe } from 'src/app/pipes/answer-color.pipe';
 export class QuizzLinesComponent {
   
   /** Quizz lines loading indicator from the parent */
-  @Input() areQuizzLinesLoading: boolean | null = false;
+  areQuizzLinesLoading = input<boolean | null>(false);
 
   /** Quizz lines from the parent */
-  @Input() quizLines: QuizzLineModel[] | null = [];
+  quizLines = input<QuizzLineModel[] | null>([]);
 
   /** Indicates if a question can be changed */
-  @Input() canQuestionBeChanged: boolean | null = true;
+  canQuestionBeChanged = input<boolean | null>(true);
 
   /** Indicates if quizz lines are disabled */
-  @Input() disabled: boolean | null = false;
+  disabled = input<boolean | null>(false);
 
   /** Pick answer event emitter to the parent */
-  @Output() pickAnswer = new EventEmitter<QuizzAnswerModel>();
+  pickAnswer = output<QuizzAnswerModel>();
   
   /** Ask to change the quizz line sent by the event emitter */
-  @Output() changeQuizzLine = new EventEmitter<QuizzLineModel>();
-
-  /**
-   * Emit a pick answer event to the parent
-   * @param answer the answer to emit
-   * @param question the question to emit
-   */
-  pickAnswerForQuestion(answer: string, question: string): void {
-    this.pickAnswer.emit({ answer, question });
-  }
-
-  /**
-   * Ask to change the quizz line selected
-   * @param quizLine the quizz line to change
-   */
-  onChangeQuizzLine(quizLine: QuizzLineModel): void {
-    this.changeQuizzLine.emit(quizLine);
-  }
+  changeQuizzLine = output<QuizzLineModel>();
 
 }
